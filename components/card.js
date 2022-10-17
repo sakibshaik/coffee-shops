a66572779a7cbdc22f2abde5a70914e22ec250c4
sakib shaik <sakib.shaik@ft.com>
2022-10-17 14:38:08 +0100

    adding more styling to card component

--- components/card.js
+++ components/card.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from '../styles/card.module.css'
+import cls from "classnames";
 
 const Card = (props) => {
     return     <Link href={props.href}>
         <a className={styles.cardLink}>
-            <div className={styles.container}>
+            <div className={cls("glass",styles.container)}>
                 <div className={styles.cardHeaderWrapper}>
                     <h2 className={styles.cardHeader}>{props.name}</h2>
                 </div>
