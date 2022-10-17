726c29149265629deab0b45f8fa43ab35d598b99
sakib shaik <sakib.shaik@ft.com>
2022-10-17 14:35:12 +0100

    adding more structure to card component

--- components/card.js
+++ components/card.js
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from '../styles/card.module.css'
 
 const Card = (props) => {
-    return <Link href = {props.href}>
-        <a>
-        <h2>{props.name}</h2>
-        <Image src={props.imgUrl} width={260} height={160} />
+    return     <Link href={props.href}>
+        <a className={styles.cardLink}>
+            <div className={styles.container}>
+                <div className={styles.cardHeaderWrapper}>
+                    <h2 className={styles.cardHeader}>{props.name}</h2>
+                </div>
+                <div className={styles.cardImageWrapper}>
+                    <Image
+                        className={styles.cardImage}
+                        src={props.imgUrl}
+                        width={260}
+                        height={160}
+                    />
+                </div>
+            </div>
         </a>
     </Link>
 }
