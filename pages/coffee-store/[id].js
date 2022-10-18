d9b3c8fe918a42252187b469a68bafc21f3ae962
sakib shaik <sakib.shaik@ft.com>
2022-10-18 10:22:12 +0100

    get static paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,11 +1,39 @@
 import {useRouter} from "next/router";
 import Link from "next/link";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "../../data/coffee-stores.json"
+
+export function getStaticProps(staticProps){
+    const params = staticProps.params
+    return {
+    props:{
+        coffeeStore: coffeeStoresData.find(store => {
+            return store.id == params.id
+        })
+    }
+    }
+}
+
+export function  getStaticPaths(){
+    return {
+        paths: [
+            {params: {id:"0"}},
+            {params: {id:"1"}},
+        ],
+        fallback: false,
+    }
+}
+
+
+const CoffeeStore = (props) => {
     const router = useRouter()
     return<div>Coffee Store Page {router.query.id}
     <Link href={"/"}><a>Back to Home</a></Link>
     <Link href={"/coffee-store/dynamic"}><a>Go to Page Dynamic</a></Link>
+        <p>{props.coffeeStore.name}</p>
+        <p>{props.coffeeStore.address}</p>
+        <p>{props.coffeeStore.neighbourhood}</p>
+        <p>{props.coffeeStore.websiteUrl}</p>
     </div>
 }
 
