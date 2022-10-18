0836936ddc900d9324243ffe27610facd8465f07
sakib shaik <sakib.shaik@ft.com>
2022-10-18 11:33:19 +0100

    styling rest page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,43 +1,95 @@
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
 
-import coffeeStoresData from "../../data/coffee-stores.json"
+import cls from "classnames";
 
-export function getStaticProps(staticProps){
-    const params = staticProps.params
+import coffeeStoresData from "../../data/coffee-stores.json";
+
+import styles from "../../styles/coffee-store.module.css";
+
+export async function getStaticProps(staticProps) {
+    const params = staticProps.params;
+    console.log("params", params);
     return {
-    props:{
-        coffeeStore: coffeeStoresData.find(store => {
-            return store.id == params.id
-        })
-    }
-    }
+        props: {
+            coffeeStore: coffeeStoresData.find((coffeeStore) => {
+                return coffeeStore.id.toString() === params.id; //dynamic id
+            }),
+        },
+    };
 }
 
-export function  getStaticPaths(){
+export function getStaticPaths() {
+    const paths = coffeeStoresData.map((coffeeStore) => {
+        return {
+            params: {
+                id: coffeeStore.id.toString(),
+            },
+        };
+    });
     return {
-        paths: [
-            {params: {id:"0"}},
-            {params: {id:"1"}},
-        ],
+        paths,
         fallback: true,
-    }
+    };
 }
-
 
 const CoffeeStore = (props) => {
-    const router = useRouter()
-    if(router.isFallback){
-        return <div>loading ...</div>
+    const router = useRouter();
+    if (router.isFallback) {
+        return <div>Loading...</div>;
     }
-    return<div>Coffee Store Page {router.query.id}
-    <Link href={"/"}><a>Back to Home</a></Link>
-    <Link href={"/coffee-store/dynamic"}><a>Go to Page Dynamic</a></Link>
-        <p>{props.coffeeStore.name}</p>
-        <p>{props.coffeeStore.address}</p>
-        <p>{props.coffeeStore.neighbourhood}</p>
-        <p>{props.coffeeStore.websiteUrl}</p>
-    </div>
-}
+
+    const { address, name, neighbourhood, imgUrl } = props.coffeeStore;
+
+    const handleUpvoteButton = () => {};
+
+    return (
+        <div className={styles.layout}>
+            <Head>
+                <title>{name}</title>
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.col1}>
+                    <div className={styles.backToHomeLink}>
+                        <Link href="/">
+                            <a>Back to home</a>
+                        </Link>
+                    </div>
+                    <div className={styles.nameWrapper}>
+                        <h1 className={styles.name}>{name}</h1>
+                    </div>
+                    <Image
+                        src={imgUrl}
+                        width={600}
+                        height={360}
+                        className={styles.storeImg}
+                        alt={name}
+                    />
+                </div>
+
+                <div className={cls("glass", styles.col2)}>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/places.svg" width="24" height="24" />
+                        <p className={styles.text}>{address}</p>
+                    </div>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/nearMe.svg" width="24" height="24" />
+                        <p className={styles.text}>{neighbourhood}</p>
+                    </div>
+                    <div className={styles.iconWrapper}>
+                        <Image src="/static/icons/star.svg" width="24" height="24" />
+                        <p className={styles.text}>1</p>
+                    </div>
+
+                    <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+                        Up vote!
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default CoffeeStore;
